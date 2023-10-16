cca81ab538b5190016fe7916a179ad2515e8b240
A-Alafandi <abdulrazak.alafandi@hotmail.com>
2023-10-16 18:54:18 +0200

    Feat :fetch data from Github&add spinner component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import UserResults from '../components/users/UserResults'
+function Home() {
+  return (
+    <>
+      {/* todo SEARCH COMPONENT */}
+      <UserResults />
+    </>
+  )
+}
+
+export default Home
